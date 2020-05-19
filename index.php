7f4bbbebe29d0f916cf35b2ae53c8b345dba4f4b
samrichboy <65594702+samrichboy@users.noreply.github.com>
2020-05-19 10:26:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="asset/newasset/css/bootstrap.min.css" />
+
+    
+    <title>vabtop</title>
+  </head>
+  <body>
+    <header class="header"> 
+  <?php
+  include 'hd/header.php';
+  ?>
+</header>
+
+
+<section class="mt-3">
+<div class="container">
+<div class="card">
+  <div class="card-body">
+    <div class="media">
+  <img src="asset/img/favicon.png" class="align-self-start mr-3" alt="...">
+  <div class="media-body">
+    <h5 class="mt-0">Top-aligned media</h5>
+    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+    <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+  </div>
+</div>
+  </div>
+</div>
+</div>
+</section>
+
+<section class="mt-2">
+<div class="container">
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+</section>
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="asset/js/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script>
+      jQuery(document).ready(function(e){
+
+        //alert('welcome');
+      })
+
+    </script>
+    <script src="asset/js/bootstrap.js" type="text/javascript"></script>
+    <script src="asset/js/jquery.js" type="text/javascript"></script>
+    <script src="asset/js/bootstrap.min.js" type="text/javascript"></script>
+</body>
+</html>
